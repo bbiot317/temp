021cc3b1a4eb1fb70c54995d679e3a617ca02ca6
bbiot317 <101791578+bbiot317@users.noreply.github.com>
2022-04-27 14:59:22 +0900

    Add files via upload
    23

--- 13-18.py
+++ 13-18.py
@@ -1,176 +1,122 @@
+from wand import *
 from tkinter import *
 from tkinter.filedialog import *
 from tkinter.simpledialog import *
-from wand.image import *
 
-# 함수 정의 부분
-def displayImage(img, width, height) :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-
-    window.geometry(str(width)+"x"+str(height))
-    if canvas != None :
+def displayimage(img, width, height):
+    global wd, canvas, paper, photo, photo2, orix, oriy
+    wd.geometry(str(width)+'x'+str(height))
+    if canvas != None:
         canvas.destroy()
-
-    canvas = Canvas(window, width=width, height=height)
+    
+    canvas = Canvas(wd, width=width, height=height)
     paper=PhotoImage(width=width, height=height)
-    canvas.create_image( (width/2, height/2), image=paper, state="normal")
+    canvas.create_image( (width/2, height/2), image=paper, state='normal') 
 
     blob = img.make_blob(format='RGB')
-    for i in range(0, width) :
-        for k in range(0, height) :
-            r = blob[(i*3*width)+(k*3) + 0]
-            g = blob[(i*3*width)+(k*3) + 1]
-            b = blob[(i*3*width)+(k*3) + 2]
-            paper.put("#%02x%02x%02x" % (r,g,b) , (k, i))
-
+    for i in range(0, width):
+        for k in range(0, height):
+            r = blob[(i*3*width)+(k*3)+0]
+            g = blob[(i*3*width)+(k*3)+1]
+            b = blob[(i*3*width)+(k*3)+2]
+            paper.put("#%02x%02x%02x" % (r,g,b) , (k,i))
     canvas.pack()
 
-def func_zoomin() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    scale = askinteger("확대배수", "확대할 배수를 입력하세요", minvalue=2, maxvalue=4)
-    photo2 = photo.clone()
-    photo2.resize( int(oriX * scale) , int(oriY * scale) )
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
+def func_open():
+    global wd, canvas, paper, photo, photo2, orix, oriy
+    readfp = askopenfilename(parent=wd, filetypes=( ('gifs','*.gif'), ('alls', '*.*') ))
+    photo = Image(filename=readfp)
+    orix = photo.width
+    oriy = photo.height
 
-def func_zoomout() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    scale = askinteger("축소", "축소할 배수를 입력하세요", minvalue=2, maxvalue=4)
     photo2 = photo.clone()
-    photo2.resize( int(oriX / scale) , int(oriY / scale) )
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
+    newx = photo2.width
+    newy = photo2.height
+    displayimage(photo2, newx, newy)
+def save():
+    global wd, canvas, paper, photo, photo2, orix, oriy
 
-def func_mirror1() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
+    if photo2 == None:
+        return
+    saveFp = asksaveasfile(parent=wd, mode='w', defaultextension='.jpg', filetypes=(('alls', '*.*')))
+    savephoto = photo2.convert('jpg')
+    savephoto.save(filename=saveFp.name)
+def func_exit():
+    pass
+def func_zoomin():
+    global wd, canvas, paper, photo, photo2, orix, oriy
+    scale = askinteger('확대배수', '입력', minvalue=2, maxvalue=4)
+    photo2 = photo.clone()
+    photo2.resize( int(orix * scale), int (oriy * scale))
+    newx = photo2.width
+    newy = photo2.height
+    displayimage(photo2, newx, newy)
+def func_zoomout():
+    global wd, canvas, paper, photo, photo2, orix, oriy
+    scale = askinteger('츅소배수', '입력', minvalue=2, maxvalue=4)
+    photo2 = photo.clone()
+    photo2.resize( int(orix/ scale), int (oriy / scale))
+    newx = photo2.width
+    newy = photo2.height
+    displayimage(photo2, newx, newy)
+def func_flip():
+    global wd, canvas, paper, photo, photo2, orix, oriy
     photo2 = photo.clone()
     photo2.flip()
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
-
-def func_mirror2() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
+    newx = photo2.width
+    newy = photo2.height
+    displayimage(photo2, newx, newy)
+def func_flop():
+    global wd, canvas, paper, photo, photo2, orix, oriy
     photo2 = photo.clone()
     photo2.flop()
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
-
-def func_rotate() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    degree = askinteger("회전", "회전할 각도를 입력하세요", minvalue=0, maxvalue=360)
+    newx = photo2.width
+    newy = photo2.height
+    displayimage(photo2, newx, newy)
+def func_rotate():
+    global wd, canvas, paper, photo, photo2, orix, oriy
+    degree = askinteger('회전','각도',minvalue=0, maxvalue=360)
     photo2 = photo.clone()
     photo2.rotate(degree)
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
+    newx = photo2.width
+    newy = photo2.height
+    displayimage(photo2, newx, newy)
 
-def func_bright() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    value = askinteger("밝게", "값을 입력하세요(100~200)", minvalue=100, maxvalue=200)
-    photo2 = photo.clone()
-    photo2.modulate(value, 100, 100)
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
 
-def func_dark() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    value = askinteger("어둡게", "값을 입력하세요(0~100)", minvalue=0, maxvalue=100)
-    photo2 = photo.clone()
-    photo2.modulate(value, 100, 100)
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
+photo,canvas,paper=None,None,None
 
-def func_clear() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    value = askinteger("선명하게", "값을 입력하세요(100~200)", minvalue=100, maxvalue=200)
-    photo2 = photo.clone()
-    photo2.modulate(100, value, 100)
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
 
-def func_unclear() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    value = askinteger("탁하게", "값을 입력하세요(0~100)", minvalue=0, maxvalue=100)
-    photo2 = photo.clone()
-    photo2.modulate(100, value, 100)
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
+wd = Tk()
+wd.geometry("300x200")
+wd.title('aaaaaaaa')
 
-def func_bw() :
-    pass
+mainMenu = Menu(wd)
+wd.config(menu=mainMenu)
 
-def func_open() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
-    readFp = askopenfilename(parent=window, filetypes=(("모든 그림 파일", "*.jpg;*.jpeg;*.bmp;*.png;*.tif;*.gif"),  ("모든 파일", "*.*") ))
-    photo = Image(filename=readFp)
-    oriX = photo.width
-    oriY = photo.height
+childMenu = Menu(mainMenu)
+mainMenu.add_cascade(label='파일',menu=childMenu)
 
-    photo2 = photo.clone()
-    newX = photo2.width
-    newY = photo2.height
-    displayImage(photo2, newX, newY)
+childMenu.add_command(label='열기', command=func_open)
+childMenu.add_separator()
+childMenu.add_command(label='저장', command=save)
+childMenu.add_separator()
+childMenu.add_command(label='종료', command=exit)
 
-def func_save() :
-    global window,canvas, paper, photo, photo2, oriX, oriY
+imageMenu = Menu(mainMenu)
+mainMenu.add_cascade(label='처리1',menu=imageMenu)
 
-    if photo2 == None :
-        return
-    saveFp = asksaveasfile(parent=window, mode="w", defaultextension=".jpg", filetypes=(("JPG 파일", "*.jpg;*.jpeg"),  ("모든 파일", "*.*") ))
-    savePhoto = photo2.convert("jpg")
-    savePhoto.save(filename=saveFp.name)
+imageMenu.add_command(label='확대', command=func_zoomin)
+imageMenu.add_separator()
+imageMenu.add_command(label='축소',command=func_zoomout)
+imageMenu.add_separator()
+imageMenu.add_command(label='상하반전',command=func_flip)
+imageMenu.add_command(label='좌우반전',command=func_flop)
+imageMenu.add_separator()
+imageMenu.add_command(label='회전',command=func_rotate)
 
-def func_exit() :
-    window.quit()
-    window.destroy()
 
-# 변수 선언 부분
-window,canvas, paper=None, None, None
-photo, photo2=None, None
-oriX,oriY= 0,0
+plabel = Label(wd)
+plabel.pack(expand=1, anchor=NE)
 
-# 메인 코드  부분
-window = Tk()
-window.geometry("256x256")
-window.title("미니 포토샵(Ver 0.1)")
+wd.mainloop()
 
-mainMenu = Menu(window)
-window.config(menu=mainMenu)
-photo = PhotoImage()
-pLabel = Label(window, image=photo)
-pLabel.pack( expand=1, anchor=CENTER)
-
-fileMenu = Menu(mainMenu)
-mainMenu.add_cascade(label="파일", menu=fileMenu)
-fileMenu.add_command(label="파일 열기", command=func_open, accelerator="Ctrl+O")
-fileMenu.add_command(label="파일 저장", command=func_save, accelerator="Ctrl+S")
-fileMenu.add_separator()
-fileMenu.add_command(label="프로그램 종료", command=func_exit, accelerator="Ctrl+X")
-
-image1Menu = Menu(mainMenu)
-mainMenu.add_cascade(label="이미지 처리(1)", menu=image1Menu)
-image1Menu.add_command(label="확대", command=func_zoomin)
-image1Menu.add_command(label="축소", command=func_zoomout)
-image1Menu.add_separator()
-image1Menu.add_command(label="상하 반전", command=func_mirror1)
-image1Menu.add_command(label="좌우 반전", command=func_mirror2)
-image1Menu.add_command(label="회전", command=func_rotate)
-
-image2Menu = Menu(mainMenu)
-mainMenu.add_cascade(label="이미지 처리(2)", menu=image2Menu)
-image2Menu.add_command(label="밝게", command=func_bright)
-image2Menu.add_command(label="어둡게", command=func_dark)
-image2Menu.add_separator()
-image2Menu.add_command(label="선명하게", command=func_clear)
-image2Menu.add_command(label="탁하게", command=func_unclear)
-image2Menu.add_separator()
-image2Menu.add_command(label="흑백이미지", command=func_bw)
-window.mainloop()
